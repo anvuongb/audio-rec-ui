16fa96d171ac4999fc4c705140dfd14dfbbbdac2
anvb <anvb@vng.com.vn>
2021-06-11 16:10:37 +0700

    add tag version

--- constant/url.js
+++ constant/url.js
@@ -1,5 +1,5 @@
 export const urlBase = ""
-export const versionNumber = "v1.1.7"
+export const versionNumber = "v1.1.9"
 export const resetToken = "u8Vi7QXOE6kOssCg0GfV9V2oSuj4wvn"
 
 export default urlBase
