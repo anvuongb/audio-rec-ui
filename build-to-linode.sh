61955193c642ff1c52f98ae2d63381ae914bc1f4
anvuong <anvb@vng.com.vn>
2022-05-17 20:27:58 -0700

    update build

--- build-to-linode.sh
+++ build-to-linode.sh
@@ -1,3 +1,3 @@
-docker build . -t audio-api:$1
-docker save audio-api:$1 -o audio-api.$1
-scp audio-api.$1 root@45.79.201.32:/root/docker-images
+docker build . -t audio-ui:$1
+docker save audio-ui:$1 -o audio-ui.$1
+scp audio-ui.$1 root@45.79.201.32:/root/docker-images
